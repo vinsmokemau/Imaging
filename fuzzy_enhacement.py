dbc6d33c61b76930162f3266cd0a559aa1de3aba
Mau Munguia <maumg1196@gmail.com>
2020-10-05 13:09:44 -0500

    Add fuzzy enhancement script

--- fuzzy_enhacement.py
+++ fuzzy_enhacement.py
@@ -1,15 +1,47 @@
-"""Get the Histogram of an Image."""
+"""Fuzzy Image Enhancement."""
 from skimage import data
+import skfuzzy as fuzz
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-dark = np.zeros(256)
+img = data.camera()
+rows, columns = img.shape
 
-for pixel in range(256):
-    if pixel < 35:
-        dark = 1
-    elif pixel >= 35 and pixel <= 100:
-        dark = (100 - pixel) / 65  # (100-pixel) / (100-35)
-    else:
-        dark = 0
+# Set the range for the fuzzyfication
+pixels = np.linspace(0, 255, 256)
+
+# Fuzzyfication of gray levels
+dark = fuzz.zmf(pixels, 25, 130)
+light = fuzz.smf(pixels, 130, 230)
+gray = fuzz.gbellmf(pixels, 55, 3, 128)
+
+# Singletons values
+s1 = 30
+s2 = 120
+s3 = 220
+
+new_gray = np.zeros(256)
+
+# Defuzzyfication process
+for i in range(256):
+    new_gray[i] = (dark[i] * s1) + (gray[i] * s2) + (light[i] * s3)
+    new_gray[i] /= (dark[i] + gray[i] + light[i])
+
+fhe = np.zeros((rows, columns))
+
+# Rule-based enhancement
+for row in range(rows):
+    for column in range(columns):
+        value = img[row, column]
+        fhe[row, column] = new_gray[value]
+
+plt.figure(1)
+plt.plot(dark)
+plt.plot(light)
+plt.plot(gray)
+
+plt.figure(2)
+plt.imshow(fhe, cmap='gray')
+
+plt.show()
